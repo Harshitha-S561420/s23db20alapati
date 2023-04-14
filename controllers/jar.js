575305a1c11999ffad5d83ceb146676ff05ce734
Harshitha-S561420 <harshithaalapati7@gmail.com>
2023-04-13 23:45:41 -0500

    ss8

--- controllers/jar.js
+++ controllers/jar.js
@@ -14,13 +14,32 @@ exports.jar_list = async function(req, res) {
 /*exports.jar_list = function(req, res) {
  res.send('NOT IMPLEMENTED: jar list');
 };*/
+// Handle jar create on POST.
+
 // for a specific jar.
 exports.jar_detail = function(req, res) {
  res.send('NOT IMPLEMENTED: jar detail: ' + req.params.id);
 };
 // Handle jar create on POST.
-exports.jar_create_post = function(req, res) {
- res.send('NOT IMPLEMENTED: jar create POST');
+exports.jar_create_post = async function(req, res) {
+ //res.send('NOT IMPLEMENTED: jar create POST');
+ console.log(req.body)
+    let document = new jar();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"jar_type":"goat", "cost":12, "size":"large"}
+    document.jar_type = req.body.jar_type;
+    document.cost = req.body.cost;
+    document.size = req.body.size;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
 };
 // Handle jar delete form on DELETE.
 exports.jar_delete = function(req, res) {
@@ -30,3 +49,16 @@ exports.jar_delete = function(req, res) {
 exports.jar_update_put = function(req, res) {
  res.send('NOT IMPLEMENTED: jar update PUT' + req.params.id);
 };
+
+// VIEWS
+// Handle a show all view
+exports.jar_view_all_Page = async function(req, res) {
+    try{
+    thejar = await jar.find();
+    res.render('jar', { title: 'jar Search Results', results: thejar });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
--- routes/jar.js
+++ routes/jar.js
@@ -1,9 +1,6 @@
 var express = require('express');
+const jar_controlers= require('../controllers/jar');
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('jar', { title: 'Search Results Jar' });
-});
-
+/* GET jar */
+router.get('/', jar_controlers.jar_view_all_Page );
 module.exports = router;
