b27ebffcbdb08b201648f3a1e2cf5422b3f0dc91
Harshitha-S561420 <harshithaalapati7@gmail.com>
2023-04-14 11:28:29 -0500

    lab12

--- controllers/jar.js
+++ controllers/jar.js
@@ -17,9 +17,21 @@ exports.jar_list = async function(req, res) {
 // Handle jar create on POST.
 
 // for a specific jar.
+/*
 exports.jar_detail = function(req, res) {
  res.send('NOT IMPLEMENTED: jar detail: ' + req.params.id);
-};
+};*/
+// for a specific jar.
+exports.jar_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await jar.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
 // Handle jar create on POST.
 exports.jar_create_post = async function(req, res) {
  //res.send('NOT IMPLEMENTED: jar create POST');
@@ -45,9 +57,29 @@ exports.jar_create_post = async function(req, res) {
 exports.jar_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: jar delete DELETE ' + req.params.id);
 };
+/*
 // Handle jar update form on PUT.
 exports.jar_update_put = function(req, res) {
  res.send('NOT IMPLEMENTED: jar update PUT' + req.params.id);
+};*/
+// Handle jar update form on PUT.
+exports.jar_update_put = async function(req, res) {
+ console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+ try {
+ let toUpdate = await jar.findById( req.params.id)
+ // Do updates of properties
+ if(req.body.material) toUpdate.material = req.body.material;
+ if(req.body.cost) toUpdate.cost = req.body.cost;
+ if(req.body.colour) toUpdate.colour = req.body.colour;
+ let result = await toUpdate.save();
+ console.log("Sucess " + result)
+ res.send(result)
+ } catch (err) {
+ res.status(500)
+ res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+ }
 };
 
 // VIEWS
--- routes/jar.js
+++ routes/jar.js
@@ -2,5 +2,5 @@ var express = require('express');
 const jar_controlers= require('../controllers/jar');
 var router = express.Router();
 /* GET jar */
-router.get('/', jar_controlers.jar_view_all_Page );
+router.get('/jar/:id', jar_controlers.jar_view_all_Page );
 module.exports = router;
